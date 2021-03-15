72a309f8b2d9b49895b5cb4cc57dcab9706bd8ec
hyman-zhou <604583358@qq.com>
2021-03-15 23:58:50 +0800

    --story=add zuul gateway

---
+++ gateway-zuul-6001/src/main/java/com/csasc/serviceshop/GatewayServer_6001.java
@@ -0,0 +1,11 @@
+package com.csasc.serviceshop;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class GatewayServer_6001 {
+}
